240fdbfb28de950c93b5851c4e71b8cc56841728
2058914895 <2058914895@qq.com>
2026-07-22 13:51:54 +0800

    Add files via upload

---
+++ offtime/offtime/offtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  offtimeApp.swift
+//  offtime
+//
+//  Created by lt on 2026/7/22.
+//
+
+import SwiftUI
+
+@main
+struct offtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
